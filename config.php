0e7230651dd45756cb25aa4fdde51f53ff8c75b7
Dzubayyan Ahmad <hello@masdzub.com>
2024-10-12 22:13:38 +0700

    add git ignore

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 // YOURLS API URL and signature
-$yourls_url = 'https://yourdomain/yourls-api.php';
-$yourls_signature = 'your_signature_api';  // Replace with your actual YOURLS API signature
+$yourls_url = 'https://s1s.uk/yourls-api.php';
+$yourls_signature = 'a2edec8fc6';  // Replace with your actual YOURLS API signature
 
 // Uploadcare API Configuration
-$uploadcare_public_key = 'uploadcare_public_key'; // Replace with your Uploadcare public key
-$uploadcare_secret_key = 'uploadcare_secret_key'; // Replace with your Uploadcare secret key
+$uploadcare_public_key = 'f935a3d1c200c049ee7c'; // Replace with your Uploadcare public key
+$uploadcare_secret_key = '822167a2d871b8d47b57'; // Replace with your Uploadcare secret key
 
 ?>
